30a6c9a5f6544fbd78759d70a02a74d02b327e5c
dipti-pachpande <diptipachpande28@gmail.com>
2024-01-14 15:45:07 +0530

    first commit

---
+++ eGrocery/src/test/java/com/assignment/eGrocery/EGroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.eGrocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
